693553aaaa2ea5980621f41dcb0d606a70aa0452
Daniel0127 <rbutrono@toluca.tecnm.mx>
2020-12-13 23:11:20 -0600

    carrito

--- AccessoPaso1/Models/Item.cs
+++ AccessoPaso1/Models/Item.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using AccessoPaso1.Models;
 namespace AccessoPaso1.Models
 {
     public class Item
     {
+        public Producto Product
+        {
+            get;
+            set;
+        }
+        public int Cantidad
+        {
+            get;
+            set;
+        }
     }
 }
