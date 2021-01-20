57b02e82f85234a9321e6555a97f85287de5800d
DanButronOtero <57423908+DanButronOtero@users.noreply.github.com>
2021-01-20 00:57:24 -0600

    Add files via upload
    Controladores y vistas de Catalogo y Productos

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccessoPaso1.Models;
+
+namespace AccessoPaso1.Controllers
+{
+    
+
+    public class CatalogoController : Controller
+    {
+        private contextTienda db = new contextTienda();
+        // GET: Catalogo
+        public ActionResult Index()
+        {
+            using (db)
+            {
+                var query = from st in db.Productoes
+                    select st;
+                var listProd = query.ToList();
+                ViewBag.Listado = listProd;
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult BuscaProd(string nomBuscar)
+        {
+            ViewBag.SearchKey = nomBuscar;
+            using (db)
+            {
+                var query = from st in db.Productoes
+                            where st.nombre.Contains(nomBuscar)
+
+                            select st;
+                var listProd = query.ToList();
+                ViewBag.Listado = listProd;
+            }
+            return View();
+        }
+
+
+       
+        public ActionResult prodCategoria(int idCat,string nombre)
+        {   
+            List<Producto> mercancia = null;
+            var query = from p in db.Productoes
+                        where p.id_categoria == idCat
+                        select p;
+
+            mercancia = query.ToList();
+            ViewBag.Catego = nombre;
+            ViewBag.productos = mercancia;
+            return View();
+        }
+    }
+}
